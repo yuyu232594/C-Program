d5308d29111dbedfe2b71b50e2a9186d9ae51dff
yuyu232594 <3505248103@qq.com>
2020-05-15 07:34:54 +0800

    使用赋值法而非交换法来进行插入排序

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -6,15 +6,11 @@ template<typename T>
 
 void insertion_sort(T arr[],int n){
 	//插入排序的思想是寻找第i个元素合适的插入位置
-	
-	for (int i=1;i<n;i++){
-		for(int j=i;j>0;j--){
-			if(arr[j]<arr[j-1])
+	for(int i=1;i<n;i++){
+		for (int j=i;j>0;j--)
+			if (arr[j]<arr[j-1])
 				swap(arr[j],arr[j-1]);
-			else
-				break;
-		}
-
+			else break;
 	}
 }
 
@@ -30,13 +26,32 @@ void selection_sort(T a[],int n){
 	}
 }
 
+template<typename T>
+void improve_insertion_sort(T arr[],int n){
+	//插入排序的思想是寻找第i个元素合适的插入位置
+	for(int i=1;i<n;i++){
+		T element=arr[i];
+		int j;
+		for(j=i;j>0;j--){
+			if(arr[j-1]>element)
+				arr[j]=arr[j-1];
+			else
+				break;
+		}
+		arr[j]=element;
+	
+	}
+}
+
+
 int main(){
 	int *arr1=TestSortHelper::generateRandomArray(10,0,10);
-	int *arr2=TestSortHelper::generateRandomArray(10,10,20);
-	selection_sort(arr1,10);
-	insertion_sort(arr2,10);
-	TestSortHelper::printArray(arr1,10);
-	cout<<"------the division line----------"<<endl;
+	int *arr2=TestSortHelper::generateRandomArray(10,10,100);
+	// int arr[]={1,3,5,7,9,2,4,6,8,0};
+
+	improve_insertion_sort(arr2,10);
+	// cout<<"------the division line----------"<<endl;
 	TestSortHelper::printArray(arr2,10);
+	return 0;
 
 }
